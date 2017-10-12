aed2069dba14f447b3cbfb602bff100ee3815e4f
amandadumi <amanda.e.dumi@gmail.com>
2017-10-12 15:05:47 -0400

    updating week 2

--- week2_basics/test_answers.py
+++ week2_basics/test_answers.py
@@ -1,4 +1,25 @@
-def check_forloop(variable):
+
+
+
+def check_forloop2(start,end,step_size,value):
+    if start==10:
+        print('Your start variable is correct! Great job!')
+    else:
+        print('Your start variable is incorrect. Try again!')
+    if end==0:
+        print('Your end variable is correct! Great job!')
+    else:
+        print('Your end variable is incorrect. Try again!')
+    if step_size==-2:
+        print('Your step_size variable is correct! Great job!')
+    else:
+        print('Your step_size variable is incorrect! Try again!')
+    if value==36:
+        print('Your value variable is correct! Great job!')
+    else:
+        print('Your value variable is incorrect! Try again!')
+
+def check_forloop2(variable):
     x_temp=0
     for i in range(21):
         x_temp += (x_temp + 31)/94
@@ -6,3 +27,9 @@ def check_forloop(variable):
         print('You are correct! Great job!')
     else:
         print('You are incorrect. Try again!')
+
+def check_fuelcells(fcn):
+    if fcn=8:
+        print('Your fuel cell calculation is correct! Great job!')
+    else:
+        print('Your fuel cell calculation is incorrect! Try again!')
